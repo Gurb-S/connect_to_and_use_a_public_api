775b6c0506dc64b2ec03f7e8fd23d47969d67e26
Gurb-S <OrganicMintz@gmail.com>
2022-02-08 04:21:11 +0100

    add basic directory

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ async function getProfiles(){
     const response = await fetch('https://randomuser.me/api/?results=12');
     const data = await response.json();
     const profiles = data.results;
-
-    //const completeProfiles = profiles.map( profile => profile);
     console.log(profiles);
     return Promise.all(profiles);
 }
@@ -15,20 +13,16 @@ function displayProfiles(data){
     console.log(data);
     data.map(person => {
         directory.innerHTML += `<div class="profile">
-        <img src="${person.picture.thumbnail}" class="user_img" alt="user">
+        <img src="${person.picture.medium}" class="user_img" alt="user">
         <div class="user_info">
-            <h3 class="name">${person.name}${person.name}</h3>
+            <h3 class="name">${person.name.first} ${person.name.last}</h3>
             <p class="email">${person.email}</p>
-            <p class="address">${person.email}${person.location.country}</p>
+            <p class="address">${person.location.city}, ${person.location.country}</p>
         </div>    
         </div>`;
     })
     
 }
-
-// function mapProfiles (data) {
-//     return data.map( profile => console.log(displayProfiles(data)));
-// }
 
 getProfiles()
     .then(displayProfiles);
